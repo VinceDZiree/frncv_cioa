fb210f8e676c4fb10b872ccb794b498b3d62f186
Vincent Désiréee <v.desiree@cioa.com>
2022-11-07 15:39:15 +0100

    Spread object props

--- App/App.js
+++ App/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import AppStylesheet from './App.styles';
 
 function App(props) {
   return (
     <View>
-      <Text>Hello dude !</Text>
+      <Text style={{...AppStylesheet.textCyan, ...AppStylesheet.underline}}>
+        Hello dude !
+      </Text>
     </View>
   );
 }
